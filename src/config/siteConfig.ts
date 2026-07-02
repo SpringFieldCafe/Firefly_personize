8b4220f4add643b7b6b456bd97bcda18f899ea57
綾波あやなみ <2574703489@qq.com>
2026-07-02 17:38:11 +0800

    change main title and subtitle

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -6,10 +6,10 @@ const SITE_LANG = "zh_CN";
 
 export const siteConfig: SiteConfig = {
 	// 站点标题
-	title: "Firefly",
+	title: "Fly me to moon",
 
 	// 站点副标题
-	subtitle: "Demo site",
+	subtitle: "404 Not Found",
 
 	// 站点 URL
 	site_url:  "https://SpringFieldCafe.github.io",
